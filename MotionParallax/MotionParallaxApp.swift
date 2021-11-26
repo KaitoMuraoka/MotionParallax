0894f791b9e5187405b957ee5c7371a01680aa4a
KaitoMuraoka <kaitoboshi@icloud.com>
2021-11-26 15:14:19 +0900

    first commit

---
+++ MotionParallax/MotionParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionParallaxApp.swift
+//  MotionParallax
+//
+//  Created by 村岡海人 on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MotionParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
